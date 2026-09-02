a7845974ab1a86c7d3727827c6c14b844a199bb8
10abhishekkr-wq <10abhishekkr@gmail.com>
2026-09-03 00:19:05 +0530

    nc

---
+++ artifacts/bolso-workshop/config/config.local.php
@@ -0,0 +1,8 @@
+<?php
+putenv('BOLSO_DB_HOST=127.0.0.1');
+putenv('BOLSO_DB_PORT=3306');
+putenv('BOLSO_DB_NAME=bolso');
+putenv('BOLSO_DB_USER=root');
+putenv('BOLSO_DB_PASSWORD=');
+putenv('BOLSO_WHATSAPP=919674773475');
+putenv('BOLSO_PAYMENT_KEY=YOUR_PAYMENT_KEY');
